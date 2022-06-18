2f12bf5bd9c2b1f917f2a6ff498cd626d859425d
EllaVan <13349092513@163.com>
2022-06-18 02:08:09 +0000

    all_EMO2AU_occAU!=0_LR=0.001+0.0001atidx=10000

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def parse_args():
     parser.add_argument('--save_epoch', type=int, default=0)
     parser.add_argument('--batch_size', type=int, default=64)
 
-    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--lr', type=float, default=0.001)
     
     return parser.parse_args()
 
@@ -54,82 +54,94 @@ def run_myIdea(args):
     AU2index_dict = dict(zip(list(map(int, AU)), range(len(AU))))
     index2AU_dict = dict(zip(range(len(AU)), list(map(int, AU))))
 
-    AU_EMO_jpt = EMO2AU_cpt*1.0/len(EMO)
-    AU2EMO_cpt = []
-    for i in range(len(AU)):
-        AU2EMO_cpt.append(list(AU_EMO_jpt[:, i]/prob_AU[i]))
-    AU2EMO_cpt = np.array(AU2EMO_cpt)
-    AU2EMO_cpt = np.where(AU2EMO_cpt >= 0, AU2EMO_cpt, 1e-4)
+    EMO2AU_cpt = np.where(EMO2AU_cpt > 0, EMO2AU_cpt, 1e-4)
 
     save_pkl = {}
-    save_pkl['ori_AU2EMO'] = AU2EMO_cpt
+    save_pkl['ori_EMO2AU'] = EMO2AU_cpt
 
     # 交叉熵损失函数
     CE = nn.CrossEntropyLoss()
     acc_record = []
     err_record = []
     AU_occ = dict(zip(AU, [0]*len(AU)))
+    a = 0
+    AU = AU[:-2]
     
     for idx, (cur_item, emo_label, index) in enumerate(train_loader, 1):
-        x = []
         weight = []
-        au_item = []
+        occ_au = []
+        abs_au = []
         
         for i, au in enumerate(AU):
             if cur_item[0, au] == 1:
-                x.append(1.0)
-                au_item.append(i)
-                weight.append(AU2EMO_cpt[i, :])
+                occ_au.append(i)
+                weight.append(EMO2AU_cpt[:, i])
                 AU_occ[au] += 1
-        if emo_label == 0:
-            x.append(1.0)
-            weight.append(AU2EMO_cpt[-2, :])
-        elif emo_label == 2 or emo_label == 4:
-            x.append(1.0)
-            x.append(1.0)
-            weight.append(AU2EMO_cpt[-1, :])
-            weight.append(AU2EMO_cpt[-2, :])
-        elif emo_label == 5:
-            x.append(1.0)
-            weight.append(AU2EMO_cpt[-1, :])
+            elif cur_item[0, au] == 0:
+                abs_au.append(i)
+                weight.append(np.array([1-value for value in list(EMO2AU_cpt[:, i])]))
 
-        if len(x) != 0:
+        if len(occ_au) != 0:
+            
+            if emo_label == 0:
+                weight.append(EMO2AU_cpt[:, -2])
+                weight.append(np.array([1-value for value in list(EMO2AU_cpt[:, -1])]))
+            elif emo_label == 2 or emo_label == 4:
+                weight.append(EMO2AU_cpt[:, -2])
+                weight.append(EMO2AU_cpt[:, -1])
+            elif emo_label == 5:
+                weight.append(np.array([1-value for value in list(EMO2AU_cpt[:, -2])]))
+                weight.append(EMO2AU_cpt[:, -1])
+            else:
+                weight.append(np.array([1-value for value in list(EMO2AU_cpt[:, -2])]))
+                weight.append(np.array([1-value for value in list(EMO2AU_cpt[:, -1])]))
+
             weight = np.array(weight)
-
+            weight = np.where(weight > 0, weight, 1e-4)
             update = UpdateGraph(in_channels=1, out_channels=len(EMO), W=weight).to(device)
             optim_graph = optim.SGD(update.parameters(), lr=args.lr)
             
-            # AU_evidence = torch.tensor(x, dtype=torch.float32).unsqueeze(0).to(device)
             AU_evidence = torch.ones((1, 1)).to(device)
             cur_prob = update(AU_evidence)
             cur_pred = torch.argmax(cur_prob)
 
             err = CE(cur_prob, emo_label.to(device))
             err_record.append(utils.loss_to_float(err))
-            acc_record.append(torch.eq(cur_pred, emo_label.to(device)).type(torch.FloatTensor).mean().item())
+            acc_record.append(torch.eq(cur_pred, emo_label.to(device)).type(torch.FloatTensor).item())
 
             summary_writer.add_scalar('err', np.array(err_record).mean(), idx)
+            summary_writer.add_scalar('err_single', utils.loss_to_float(err), idx)
             summary_writer.add_scalar('acc', np.array(acc_record).mean(), idx)
 
             optim_graph.zero_grad()
             err.backward()
             optim_graph.step()
-
-            new_AU2EMO_cpt = AU2EMO_cpt
+                        
+            new_EMO2AU_cpt = EMO2AU_cpt
             update_info = update.fc.weight.grad.cpu().numpy()
-            for i, j in enumerate(au_item):
-                new_AU2EMO_cpt[j, emo_label] -= args.lr*update_info[emo_label]
-            AU2EMO_cpt_tmp = np.array(new_AU2EMO_cpt)
-            AU2EMO_cpt = np.where(AU2EMO_cpt_tmp >= 0, AU2EMO_cpt_tmp, 1e-4)
-            AU2EMO_cpt = np.where(AU2EMO_cpt_tmp <= 1, AU2EMO_cpt_tmp, 1)
+            summary_writer.add_scalar('update_info', update_info[emo_label], idx)
+            for i, j in enumerate(AU):
+                factor = 1
+                if i in occ_au:
+                    new_EMO2AU_cpt[emo_label, i] += args.lr*np.abs(update_info[emo_label]*factor)
+                else:
+                    new_EMO2AU_cpt[emo_label, i] -= args.lr*np.abs(update_info[emo_label]*factor)
+            EMO2AU_cpt_tmp = np.array(EMO2AU_cpt)
 
-    save_pkl['new_AU2EMO'] = AU2EMO_cpt
+            EMO2AU_cpt_tmp = np.where(EMO2AU_cpt_tmp > 0, EMO2AU_cpt_tmp, 1e-4)
+            EMO2AU_cpt = np.where(EMO2AU_cpt_tmp <= 1, EMO2AU_cpt_tmp, 1)
+            
+            if idx == 10000:
+                args.lr /= 10.0
+
+    save_pkl['new_EMO2AU'] = EMO2AU_cpt
     save_pkl['AU_occ'] = AU_occ
     save_pkl['train_size'] = len(train_loader)
     with open(os.path.join(args.save_path, 'results.pkl'), 'wb') as fo:
         pkl.dump(save_pkl, fo)
-    end_flag = True
-
+    fo.close()
+    print(a)
+    
 
 if __name__ == '__main__':
     cur_time = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
